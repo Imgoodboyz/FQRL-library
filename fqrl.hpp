3579fee880e6a0b47b6ccecfa1c902c11f974ad0
Imgoodboyz <giab06052011@gmail.com>
2025-09-15 12:34:48 +0700

    Initial commit:FQRL v0.0.13

---
+++ fqrl.hpp
@@ -0,0 +1,88 @@
+#ifndef header
+#define header
+#define version = "0.0.13"
+#define minimum = "c++ 17"
+#define stage = "preview"
+#define last_update = "14/9/2025"
+#define authorizer = "imgoodboy"
+#include <initializer_list>
+#include <filesystem>
+#include <string>
+#include <fstream>
+#include <stdexcept>
+#include <iostream>
+namespace fclc
+{
+    template<typename T>
+    inline T sum(std::initializer_list<T>num)
+    {
+       double total = 0;
+       for (T x : num)
+       {
+          total += x;
+       }
+       return total;
+    }
+    template<typename T>
+    inline T avg(std::initializer_list<T>num)
+    {
+       double total = 0;
+       int length = 0;
+       for (T x : num)
+       {
+          total += x;
+          length += 1;
+       }
+       return total/length;
+    }
+}
+namespace facs
+{
+    inline std::string abs(const std::string name)
+    {
+       return std::filesystem::absolute(name).string();
+    }
+    template<typename T>
+    inline T log(const T data)
+    {
+       std::filesystem::path caller(__FILE__);
+       std::filesystem::path logger = caller.parent_path()/"log.txt";
+       std::ofstream out(logger,std::ios::app);
+       out << data << std::endl;
+       if (!out) return "cant_open";
+       return "logged";
+    }
+    inline std::string read(std::string file)
+    {
+    	std::ifstream dc(abs(file));
+	if (!dc.is_open())
+	{
+	   throw std::runtime_error("cant open.file");
+	}
+	std::string out;
+	std::getline(dc, out);
+	return out;
+    }
+    inline std::string write(std::string file,std::string data)
+    {
+	std::ofstream lk(abs(file),std::ios::app);
+	if (!lk.is_open())
+	{
+	   throw std::runtime_error("cant open");
+	}
+	lk << data << std::endl;
+	return "done";
+    }
+    inline void prt(std::string data,bool line)
+    {
+        if (line == true)
+        {
+             std::cout<<data<<std::endl;
+        }
+        if (line == false)
+        {
+             std::cout<<data;
+        }
+    }
+}
+#endif
